f3ae187437caf02d884bbcd5c7b74aaf87eea5ad
CodingLime <mr.marcelcarvalho@gmail.com>
2023-01-12 16:30:22 +0000

    fix: website title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -264,4 +264,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Split Counter App</title>;
