51493a81b59988478671ec18cddae0771fe87781
CodingLime <mr.marcelcarvalho@gmail.com>
2025-02-12 21:49:52 +0000

    added page for sara and ricardo

---
+++ src/pages/sara-e-ricardo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SaraERicardo: React.FC = () => {
+    return (
+        <div style={{ backgroundColor: 'white', height: '100vh', margin: 0 }}>
+            <h1></h1>
+        </div>
+    );
+};
+
+export default SaraERicardo;
